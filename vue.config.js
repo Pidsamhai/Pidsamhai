e305276906fd5ec2b0d351fd3aec89bece714cbc
mengxyz <meng348@gmail.com>
2022-01-16 20:14:29 +0700

    [added] Added deploy actions
    - migrate HelloWorld to vue-class-component

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.BASE_HREF || "/",
   transpileDependencies: true,
-
   pluginOptions: {
     vuetify: {
       // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
